98d9a2e47703124c661abc0adae79f46b2daf4db
Ben <bmu@odoo.com>
2020-11-23 11:53:36 -0800

    Change quantity field label to piece count

--- cjg_sale/models/sale.py
+++ cjg_sale/models/sale.py
@@ -5,25 +5,22 @@ from odoo import fields, models, api
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    piece_count = fields.Integer(string='Piece Count')
     case_pack = fields.Integer(related='product_id.case_pack')
     box_count = fields.Integer(compute='_compute_box', string='Box Count', default=0, store=True)
     box_price = fields.Integer(compute='_compute_box', string='Box Price')
     package_count = fields.Integer(compute='_compute_package_count', string='Package Count', default=0, store=True)
 
-    @api.depends('piece_count', 'product_id.case_pack')
+    @api.depends('product_uom_qty', 'product_id.case_pack')
     def _compute_box(self):
         for record in self:
             if record.product_id.case_pack:
-                record.box_count = record.piece_count / record.product_id.case_pack
+                record.box_count = record.product_uom_qty / record.product_id.case_pack
             record.box_price = record.price_unit * record.product_id.case_pack
 
     @api.depends('product_uom_qty', 'product_packaging.qty')
     def _compute_package_count(self):
         for record in self.filtered(lambda sol: sol.product_packaging.qty):
                 record.package_count = record.product_uom_qty / record.product_packaging.qty
-
-
 
 class Sale(models.Model):
     _inherit = 'sale.order'
