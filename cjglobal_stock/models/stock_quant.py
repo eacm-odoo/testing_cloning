6adc440bc1cc718fd93689a6cd921546b1c7d47f
Martin Petrov <mpv@odoo.com>
2021-08-31 10:24:16 -0700

    updated changes to dev branch

--- cjglobal_stock/models/stock_quant.py
+++ cjglobal_stock/models/stock_quant.py
@@ -10,15 +10,13 @@ class StockQuant(models.Model):
 
     def _compute_available_to_sell(self):
         for record in self:
-            record.available_to_sell = record.inventory_quantity - record.reserved_quantity
-        
+            record.available_to_sell = record.quantity - record.reserved_quantity
     ''' 
         Override read_group to add available_to_sell field, similar to how is 
         done for inventory_quantity in stock_quant.py 
     '''
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         result = super(StockQuant, self).read_group(domain, fields, groupby, offset=offset, limit=limit, orderby=orderby, lazy=lazy)
-        if self._is_inventory_mode():
-            for record in result:
-                record['available_to_sell'] = record.get('inventory_quantity', 0) - record.get('reserved_quantity', 0)
+        for record in result:
+            record['available_to_sell'] = record.get('quantity', 0) - record.get('reserved_quantity', 0)
         return result
