75fe0d8edc2d8cdba4fc7921c4bd67efdbe09e54
elce-odoo <elce@odoo.com>
2022-10-11 10:46:04 -0500

    code review

--- cjg_purchase/models/purchse_order.py
+++ cjg_purchase/models/purchse_order.py
@@ -9,18 +9,18 @@ class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
     case_pack = fields.Integer(string='Case Pack', related='product_id.product_tmpl_id.case_pack')
-    unit_qty = fields.Float(string='Unit Quantity')
-    unit_price = fields.Float(string="Unit Price")
-    product_qty = fields.Float(string='Box Quantity', digits='Product Unit of Measure', required=True, compute="_compute_product_qty")
+    unit_qty_cj = fields.Float(string='Unit Quantity')
+    unit_price_cj = fields.Float(string="Unit Price")
+    product_qty = fields.Float(string='Box Quantity', digits='Product Unit of Measure', compute="_compute_product_qty")
     price_unit = fields.Float(string='Box Price', required=True, digits='Product Price', compute='_compute_price_unit')
 
-    @api.depends('unit_qty', 'case_pack')
+    @api.depends('unit_qty_cj', 'case_pack')
     def _compute_product_qty(self):
         for line in self:
-            line.product_qty = math.ceil(line.unit_qty / line.case_pack) if line.case_pack > 0 else line.unit_qty
+            line.product_qty = math.ceil(line.unit_qty_cj / line.case_pack) if line.case_pack > 0 else line.unit_qty_cj
     
-    @api.depends('price_unit', 'case_pack')
+    @api.depends('unit_price_cj', 'case_pack')
     def _compute_price_unit(self):
         for line in self:
-            line.price_unit = line.unit_price * line.case_pack
+            line.price_unit = line.unit_price_cj * line.case_pack
     
