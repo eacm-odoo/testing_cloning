59e254ac04534ee2b34dd0d0d8fd7b9ead9049db
hmpb-odoo <hmpb@odoo.com>
2022-11-01 09:59:35 -0600

    [Upgrade] Cjglobal upgrade modules from v13 to v15
    Task ID: 3045486

--- cjg_purchase/__manifest__.py
+++ cjg_purchase/__manifest__.py
@@ -8,11 +8,14 @@
         Developer: ELCE
         Ticket ID: 2948278
     """,
-    "author": "Odoo",
+    "author": "Odoo PS",
     "website": "https://www.odoo.com",
     "category": "Custom Development",
-    "version": "1.0",
-    "depends": ['cjg_sale_stock'],
+    "version": "1.1.0",
+    "depends": [
+        'cjg_sale_stock',
+        'purchase',
+        ],
     "data": [
         'views/purchase_order_views.xml',
     ],
