80768fa84b84876614e72b5ababc9f598de38229
EduardoCedillo(eacm) <eacm@odoo.com>
2022-11-09 09:40:24 -0600

    [IMP] cjg_purchase: improve comments with docstring
    	-move comments to use Docstring for better documentation
    
    TASK ID: 3057120

--- cjg_purchase/models/stock_rule.py
+++ cjg_purchase/models/stock_rule.py
@@ -4,8 +4,8 @@ from odoo import api, models
 class StockRule(models.Model):
     _inherit = 'stock.rule'
 
-    #override _update_purchase_order_line and add unit_qty and unit_price_cj to the values
     def _update_purchase_order_line(self, product_id, product_qty, product_uom, company_id, values, line):
+        """override _update_purchase_order_line and add unit_qty and unit_price_cj to the values"""
         values = super()._update_purchase_order_line(product_id, product_qty, product_uom, company_id, values, line)
         values.update({
             'unit_qty_cj': line.unit_qty_cj + product_qty * product_id.case_pack,
@@ -13,13 +13,12 @@ class StockRule(models.Model):
         })
         return values
 
-    #override _prepare_purchase_order_line and add unit_qty and unit_price_cj to the values
     @api.model
     def _prepare_purchase_order_line(self, product_id, product_qty, product_uom, company_id, values, po):
+        """override _prepare_purchase_order_line and add unit_qty and unit_price_cj to the values"""
         values = super()._prepare_purchase_order_line(product_id, product_qty, product_uom, company_id, values, po)
         values.update({
             'unit_qty_cj': product_qty * product_id.case_pack,
             'unit_price_cj': values['price_unit'],
         })
-        return values
-        
+        return values 
