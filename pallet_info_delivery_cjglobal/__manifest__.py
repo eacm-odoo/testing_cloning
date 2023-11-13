c3cbff7efea44ab2bce2f9becdc1d9c37cec0ca7
ralb-odoo <ralb@odoo.com>
2023-11-13 12:08:22 -0600

    [ADD] module: add pallet info to the delivery
    Module to add pallet information to the delivery transfer,
    On the package reference (stock.quant.package):
    - Pallet Height
    - Pallet Width
    - Pallet Length
    - Cubic feet: height * width * length
    On the delivery transfer (stock.picking):
    - Pallet count: total packages packed
    - Carton count: total of 'done' quantities in THAT delivery.
    - Max Height: the highest number entered under Pallet Height of the packages in the line
    - Total Cuf: total cubic feet calculated by adding the cubic feet per pallet (package)
    
    Link to task: [3478728](https://www.odoo.com/web#id=3478728&cids=17&model=project.task&view_type=form)
    
    Link to [demo](https://www.youtube.com/watch?v=5pvP1SqtXPw)

---
+++ pallet_info_delivery_cjglobal/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    'name': "CJ Global: add pallet information to the delivery transfer",
+    'summary':
+        """
+        Module to add pallet information to the delivery transfer. 
+        """,
+    'description':
+        """
+        Module to add pallet information to the delivery transfer, 
+        On the package reference (stock.quant.package):
+        - Pallet Height
+        - Pallet Width
+        - Pallet Length
+        - Cubic feet
+        On the delivery transfer (stock.picking):
+        - Pallet count: total packages packed 
+        - Carton count: total of 'done' quantities in that delivery.
+        - Max Height: the highest number entered under Pallet Height of the packages in the line
+        - Total Cuf: total cubic feet calculated by adding the cubic feet per pallet (package)
+        Developer: ralb
+        Task ID: 3478728
+        Link to task: https://www.odoo.com/web#id=3478728&cids=17&model=project.task&view_type=form
+        """,
+    'author': "Odoo Inc.",
+    'license': 'OPL-1',
+    'website': "https://www.odoo.com",
+    'category': 'Custom Modules',
+    'version': '1.0.0',
+    'depends': ['stock', 'delivery'],
+    'data': [
+        'views/stock_picking_views_cjglobal.xml',
+        'views/stock_quant_views_cjglobal.xml',
+    ]
+}
