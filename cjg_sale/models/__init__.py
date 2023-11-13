6a4a20977ee51f91ad22037f7d18cbca4ace6273
ralb-odoo <ralb@odoo.com>
2023-11-13 12:08:22 -0600

    [ADD] module: add pallet info to the delivery
    Module to add pallet information to the delivery transfer,
    On the package reference (stock.quant.package):
    - Pallet Height
    - Pallet Width
    - Pallet Length
    - Cubic feet: height * width * length
    On the delivery transfer (stock.picking):
    - Pallet count: total packages packed
    - Carton count: total of 'done' quantities in THAT delivery.
    - Max Height: the highest number entered under Pallet Height of the packages in the line
    - Total Cuf: total cubic feet calculated by adding the cubic feet per pallet (package)
    
    Link to task: [3478728](https://www.odoo.com/web#id=3478728&cids=17&model=project.task&view_type=form)

--- cjg_sale/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import product
-from . import sale
-from . import stock
-from . import sale_order_line
--- cjg_sale/models/product.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class Product(models.Model):
-    _inherit = 'product.template'
-
-    case_pack = fields.Integer(string='Case Pack') 
