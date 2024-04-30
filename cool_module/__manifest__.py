db940f141aa7c21c90e6b2268b9b9329eb151818
eacm-odoo <eacm@odoo.com>
2024-04-30 14:53:27 -0600

    add manifest

---
+++ cool_module/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': 'CJ Global: Available to sell field',
+    'summary': '''
+        Adds 'Available to Sell' field to Inventory Report List
+    ''',
+    'description': '''
+        MPV - TASK ID: 2470112
+        Adds a new field to Inventory Report List called 'Available to sell'
+        that shows the difference between on hand quantity and the reserved
+        quantity
+    ''',
+    'license': 'OPL-1',
+    'author': 'Odoo PS',
+    'website': 'https://www.odoo.com',
+    'category': 'Development Services/Custom Development',
+    'version': '2.1.0',
+    'depends': ['stock'],
+    'data': [
+        'views/stock_quant_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
